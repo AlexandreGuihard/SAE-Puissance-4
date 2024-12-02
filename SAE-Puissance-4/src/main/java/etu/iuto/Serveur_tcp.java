59e38b69f9680f0d872002e2489c4ea3f838523b
Renaudin clement <cronisosdev@gmail.com>
2024-12-02 16:56:58 +0100

    mise en place

--- SAE-Puissance-4/src/main/java/etu/iuto/Serveur_tcp.java
+++ SAE-Puissance-4/src/main/java/etu/iuto/Serveur_tcp.java
@@ -1,6 +1,19 @@
 package etu.iuto;
-
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Serveur_tcp {
+
+    public static void main(String[] args) throws IOException{
+
+        try{
+            ServerSocket se = new ServerSocket();
+        }
+        catch (Exception e){
+            System.out.println("[erreur]" + e);
+
+        }
+        
+    }
     
 }
